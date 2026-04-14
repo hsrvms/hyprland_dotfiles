0bb43c9c368a8e7f94ae7506c6d32e596a095f55
hsrvms <59848940+hsrvms@users.noreply.github.com>
2026-04-14 22:45:23 +0300

    general adjustment

--- fish/config.fish
+++ fish/config.fish
@@ -8,5 +8,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 abbr -a fzf "fzf --ansi"
 abbr -a hxf 'helix $(fzf --ansi -m --preview="bat --color=always {}")'
+abbr -a cs "gh cs"
 
 alias hx helix
+alias zed zeditor
